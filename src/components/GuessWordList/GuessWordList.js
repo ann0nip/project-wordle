b8879c06bd34ab6fe527ffb054cdb1251380e5a3
Juan Martin Gimenez <gimenezyunis@gmail.com>
2023-11-01 23:14:36 -0300

    Exercise three

--- src/components/GuessWordList/GuessWordList.js
+++ src/components/GuessWordList/GuessWordList.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Guess from '../Guess/Guess';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessWordList({ words = [] }) {
     return (
         <div className="guess-results">
-            {words.map((word) => (
-                <p key={word.id} className="guess">
-                    {word.label}
-                </p>
+            {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+                <Guess key={num} word={words[num]} />
             ))}
         </div>
     );
